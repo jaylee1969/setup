b9dd022c2814a002c76096649485e5f74ab5840d
Ubuntu <ubuntu@ip-10-137-62-220.ec2.internal>
2013-07-17 01:57:56 +0000

    Updated how I setup my vim environment, now based on github instead of hardcoded

--- setup.sh
+++ setup.sh
@@ -45,6 +45,25 @@ ln -sb dotfiles/.bashrc .
 ln -sb dotfiles/.bashrc_custom .
 ln -sf dotfiles/.emacs.d .
 
-git clone https://github.com/jaylee1969/vimenv.git
-ln -sb vimenv ~/.vim
-ln -sb vimenv/.vimrc ~/.vimrc
+# Install pathogen.vim to simplify deployment of vim plugins
+mkdir -p ~/.vim/autoload ~/.vim/bundle; \
+    curl -Sso ~/.vim/autoload/pathogen.vim \
+        https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+# Install SnipMate.vim
+cd ~/.vim/bundle
+git clone https://github.com/tomtom/tlib_vim.git
+git clone https://github.com/MarcWeber/vim-addon-mw-utils.git
+git clone https://github.com/garbas/vim-snipmate.git
+# Install surround.vim
+git clone https://github.com/tpope/vim-surround.git
+# Install l9.vim
+git clone https://github.com/vim-scripts/L9.git
+# Install fuzzy finder
+git clone https://github.com/vim-scripts/FuzzyFinder.git
+# Install example snippets files
+git clone https://github.com/honza/vim-snippets.git
+
+# install custom vim environment
+cd ~
+git clone https://github.com/jaylee1969/mydotfiles.git
+ln -sf mydotfiles/.vimrc .
